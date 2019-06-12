a29b599bcafbe1ba0cc761c1282fd23f06cd90cb
Taylor Doyle <tcd56@cornell.edu>
2019-06-12 18:17:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(array_of_calls)# code an argument here
   array_of_calls.any? {|word| word.lenth > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(ingredients)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   ingredients.find do |cheese|
